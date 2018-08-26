afbfe197c44f7b750cc1b361c2d615b7d5e83ad1
Rai <wellington.gmr@gmail.com>
2018-08-26 16:56:47 -0300

    Produto associção muitos para muitos

--- src/main/java/br/com/rai/CursomcApplication.java
+++ src/main/java/br/com/rai/CursomcApplication.java
@@ -1,8 +1,6 @@
 package br.com.rai;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.rai.domain.Categoria;
+import br.com.rai.domain.Produto;
 import br.com.rai.repositories.CategoriaRepository;
+import br.com.rai.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository repoCategoria;
+	
+	@Autowired
+	private ProdutoRepository repoProduto;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,6 +30,18 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria c1 = new Categoria(null, "Informatica");
 		Categoria c2 = new Categoria(null, "Escritorio");
 		
-		this.repo.saveAll(Arrays.asList(c1, c2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 50.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
+		this.repoCategoria.saveAll(Arrays.asList(c1, c2));
+		this.repoProduto.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
