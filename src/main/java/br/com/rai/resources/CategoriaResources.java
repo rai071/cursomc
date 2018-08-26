df01e02543c02630a0cac1e49ee051d21485bf8a
Rai <wellington.gmr@gmail.com>
2018-08-26 14:50:31 -0300

    Testando primeira classe de dominio Categoria

--- src/main/java/br/com/rai/resources/CategoriaResources.java
+++ src/main/java/br/com/rai/resources/CategoriaResources.java
@@ -1,8 +1,13 @@
 package br.com.rai.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rai.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,7 +15,15 @@ public class CategoriaResources {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	private String listar() {
-		return "Rest Configurado";
+	private List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informatica");
+		Categoria c2 = new Categoria(2, "Informatica");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 }
