06871dfcd00ccd19f83ce7a7b1265c2b190e2e91
Rai <wellington.gmr@gmail.com>
2018-08-26 14:35:18 -0300

    Rest testado

--- src/main/java/br/com/rai/resources/CategoriaResources.java
+++ src/main/java/br/com/rai/resources/CategoriaResources.java
@@ -1,5 +1,16 @@
 package br.com.rai.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
 public class CategoriaResources {
 
+	
+	@RequestMapping(method=RequestMethod.GET)
+	private String listar() {
+		return "Rest Configurado";
+	}
 }
