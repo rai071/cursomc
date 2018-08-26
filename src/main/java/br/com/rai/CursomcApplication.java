06faf49c59a0d5ccc73cece7920a55f5fd54389e
Rai <wellington.gmr@gmail.com>
2018-08-26 16:29:19 -0300

    Criando operação de instanciação

--- src/main/java/br/com/rai/CursomcApplication.java
+++ src/main/java/br/com/rai/CursomcApplication.java
@@ -1,12 +1,32 @@
 package br.com.rai;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rai.domain.Categoria;
+import br.com.rai.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informatica");
+		Categoria c2 = new Categoria(null, "Escritorio");
+		
+		this.repo.saveAll(Arrays.asList(c1, c2));
 	}
 }
